6d9d4e21bd1b11c22f51c378d2d6b760dd34a59a
yololaxe <74353812+yololaxe@users.noreply.github.com>
2024-05-21 15:32:26 +0200

    hfgh

---
+++ ConsoleAppCyberVoillier/ConsoleAppCyberVoillier/Course.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleAppCyberVoillier;
+
+public class Course
+{
+    private VoilierInscrit[] inscrits;
+    private VoilierCourse[] enCourse;
+
+    public VoilierInscrit[] Inscrits
+    {
+        get => inscrits;
+        set => inscrits = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public VoilierCourse[] EnCourse
+    {
+        get => enCourse;
+        set => enCourse = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public Course(VoilierInscrit[] inscrits, VoilierCourse[] enCourse)
+    {
+        Inscrits = inscrits;
+        EnCourse = enCourse;
+    }
+}
