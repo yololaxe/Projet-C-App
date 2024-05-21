aed0581f605dc157afc192ae3eb3ad632e4b3451
Rediumtv <rediumtv@gmail.com>
2024-05-21 14:46:32 +0200

    ajout pénalité

---
+++ ConsoleAppCyberVoillier/ConsoleAppCyberVoillier/Penalite.cs
@@ -0,0 +1,35 @@
+namespace ConsoleAppCyberVoillier;
+
+public class Penalite
+{
+    private string id;
+    private double duree;
+    private string desc; //raison de la penalite
+    
+    //properties
+    public string Id
+    {
+        get => id;
+        set => id = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public double Duree
+    {
+        get => duree;
+        set => duree = value;
+    }
+
+    public string Desc
+    {
+        get => desc;
+        set => desc = value ?? throw new ArgumentNullException(nameof(value));
+    }
+    
+    //constructor
+    public Penalite(string id, double duree, string desc)
+    {
+        this.id = id;
+        this.duree = duree;
+        this.desc = desc;
+    }
+}
