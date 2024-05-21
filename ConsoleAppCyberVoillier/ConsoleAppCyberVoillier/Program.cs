2bc0406149e5ec47f9469e36612e81c74c605233
Rediumtv <rediumtv@gmail.com>
2024-05-21 14:10:19 +0200

    21/05

---
+++ ConsoleAppCyberVoillier/ConsoleAppCyberVoillier/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace ConsoleAppCyberVoillier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne vincent = new Personne(1, "Jorland", "Vincent", "Skipper");
+            Personne Romain = new Personne(2, "Fouletier", "Romain", "Patate");
+            Sponsor Logitech = new Sponsor(1, "Logitech");
+            Sponsor Razer = new Sponsor(1, "Razer");
+            VoilierInscrit voilier1 = new VoilierInscrit(1, "0258", [vincent, Romain], [Logitech, Razer]);
+            Console.WriteLine("code : " + voilier1.Code + " id : " + voilier1.Id + "capitaine : " + voilier1.Equipage[0].Nom + " sponsorisé par : " + voilier1.Entreprise[0].Nom);
+        }
+    }
+}
