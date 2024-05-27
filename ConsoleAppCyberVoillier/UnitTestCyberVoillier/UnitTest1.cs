18343c61be6e87024eaad3563fd1d5bd5207cca8
NAIR0M <116349996+NAIR0M@users.noreply.github.com>
2024-05-27 17:13:16 +0200

    test unitaire 1.0

---
+++ ConsoleAppCyberVoillier/UnitTestCyberVoillier/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace UnitTestCyberVoillier;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
